fa722d65b5ae28c216ee8a8ea3fc9122ef9d3c46
Spinotauchus2020 <75925811+Spinotauchus2020@users.noreply.github.com>
2022-03-28 21:18:02 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+OK = "My first console project";
+First_name = "Ranbir ";
+Last_name = "Kapoor";
+a = 10;
+b = 5;
+add = First_name + Last_name ;
+add2 = a + b ;
+console.log(OK);
+console.log(add);
+console.log(add2);
+console.log(a);
+console.log(b);
